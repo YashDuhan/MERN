84df916c81294a970cb7658f1e28a99683943992
Yash Duhan <70028651+YashDuhan@users.noreply.github.com>
2024-09-23 06:10:39 +0530

    useContext

--- 03. React/5. Routing, Prop Drilling, Context API/routing-propDrilling-contextAPI/src/App.jsx
+++ 03. React/5. Routing, Prop Drilling, Context API/routing-propDrilling-contextAPI/src/App.jsx
@@ -1,52 +1,5 @@
-import { useContext, useState } from "react";
-import { CountContext } from "./context";
-
 function App() {
-  const [count, setCount] = useState(0);
-
-  // Wrap anyone that wants to use the teleported value inside a provider
-  return (
-    <div>
-      <CountContext.Provider value={count}>
-        <Count setCount={setCount} />
-      </CountContext.Provider>
-    </div>
-  );
-}
-
-function Count({ setCount }) {
-  return (
-    <div>
-      <CountRenderer />
-      <Buttons setCount={setCount} />
-    </div>
-  );
-}
-
-function CountRenderer() {
-  const count = useContext(CountContext);
-  return <div>{count}</div>;
-}
-
-function Buttons({ setCount }) {
-  const count = useContext(CountContext);
-  return (
-    <div>
-      <button
-        onClick={() => {
-          setCount(count + 1);
-        }}>
-        Increase
-      </button>
-
-      <button
-        onClick={() => {
-          setCount(count - 1);
-        }}>
-        Decrease
-      </button>
-    </div>
-  );
+  return <>Hello World</>;
 }
 
 export default App;
